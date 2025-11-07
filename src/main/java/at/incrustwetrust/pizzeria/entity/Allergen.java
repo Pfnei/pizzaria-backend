15b1740b352150d95f93fa34f90d7508ce86bcbc
Dany <q845999@gmail.com>
2025-11-07 10:39:32 +0100

    Adds MapStruct and Lombok dependencies
    Adds MapStruct for easier DTO mapping and Lombok to reduce boilerplate code.
    
    Configures the maven compiler plugin to process annotations for both libraries.

--- src/main/java/at/incrustwetrust/pizzeria/entity/Allergen.java
+++ src/main/java/at/incrustwetrust/pizzeria/entity/Allergen.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
